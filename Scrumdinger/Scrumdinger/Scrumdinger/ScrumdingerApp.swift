d2b8e452658ed26cc515a26356f488b5bedaf56c
osxgirl <osxgirl@mac.com>
2021-10-11 19:24:15 -0400

    CardView

---
+++ Scrumdinger/Scrumdinger/Scrumdinger/ScrumdingerApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScrumdingerApp.swift
+//  Scrumdinger
+//
+//  Created by heather marie haas on 10/11/21.
+//
+
+import SwiftUI
+
+@main
+struct ScrumdingerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            MeetingView()
+        }
+    }
+}
